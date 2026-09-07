4446f561ada87890ebcfcb3be88a9737fb217037
Maicol Angelicchio <maicol.angelicchio.91@gmail.com>
2026-09-07 23:03:54 +0200

    feat: add Article and Category domain models

---
+++ Chronicle/Models/Domain/Category.cs
@@ -0,0 +1,9 @@
+using System.ComponentModel.DataAnnotations;
+namespace Chronicle.Models.Domain;
+public class Category
+{
+    public long Id {get; set;}
+    [Required]
+    public string Name {get;set;} = string.Empty;
+    public ICollection<Article> Articles {get;set;} = new List<Article>();
+}
