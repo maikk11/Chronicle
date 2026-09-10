dad3bde80b0550736b512f43a06e8a1d8839f877
Maicol Angelicchio <maicol.angelicchio.91@gmail.com>
2026-09-10 22:25:19 +0200

    feat: add article and category DTOs

---
+++ Chronicle/Models/ViewModels/ArticleDto.cs
@@ -0,0 +1,16 @@
+using Chronicle.Models.Domain;
+using Microsoft.AspNetCore.Identity;
+
+namespace Chronicle.Models.ViewModels;
+public class ArticleDto
+{
+    public long Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Subtitle { get; set; } = string.Empty;
+    public string Body { get; set; } = string.Empty;
+    public DateTime? PublishDate { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public bool? IsAccepted { get; set; }
+    public IdentityUser? User { get; set; }
+    public Category? Category { get; set; }
+}
